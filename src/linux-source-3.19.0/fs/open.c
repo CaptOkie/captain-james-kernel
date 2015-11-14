6db395e74f552dce6fef55e0454e1fd71b04d4d0
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-11-14 18:12:59 -0500

    thing

--- src/linux-source-3.19.0/fs/open.c
+++ src/linux-source-3.19.0/fs/open.c
@@ -1009,7 +1009,7 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 		return fd;
 
 	tmp = getname(filename);
-	printk("Filename: %s", filename);
+	printk("Filename: %s\n", filename);
 	if (IS_ERR(tmp))
 		return PTR_ERR(tmp);
 
