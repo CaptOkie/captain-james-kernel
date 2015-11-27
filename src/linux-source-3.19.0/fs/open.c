29039ddf9776dbf521ee4f73fcb537b8a14f3fad
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-11-26 19:55:41 -0500

    A thing

--- src/linux-source-3.19.0/fs/open.c
+++ src/linux-source-3.19.0/fs/open.c
@@ -46,7 +46,6 @@
 #include <linux/audit.h>
 #include <linux/vmalloc.h>
 #include <linux/posix_acl_xattr.h>
-// #include <linux/dcache.h>
 
 #include "internal.h"
 
@@ -1151,7 +1150,8 @@ retry:
 long allow_open(struct file* f, const char* __user filename)
 {
     if (strcmp("crazy/text.txt", filename) == 0) {
-        for (struct dentry* d = f->f_path.dentry; d; d = d->d_parent)
+        struct dentry* d;
+        for (d = f->f_path.dentry; d; d = d->d_parent)
         printk("File Path: %s\n", d->d_name.name);
     }
 
