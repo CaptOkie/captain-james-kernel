062ab5bc7e43f3fd45016410e70c95f199e40d9f
TheBakedPotato <TheBakedPotato@users.noreply.github.com>
2015-11-26 19:47:11 -0500

    A thing

--- src/linux-source-3.19.0/fs/open.c
+++ src/linux-source-3.19.0/fs/open.c
@@ -1150,7 +1150,8 @@ retry:
 long allow_open(struct file* f, const char* __user filename)
 {
     if (strcmp("crazy/text.txt", filename) == 0) {
-        printk("File Path: %s\n", f->f_path.dentry->d_parent->d_name.name);
+        for (dentry* d = f->f_path.dentry; d; d = d->d_parent)
+        printk("File Path: %s\n", d->d_name.name);
     }
 
     return 0;
